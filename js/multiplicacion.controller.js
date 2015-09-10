f638b492d61c8e75bfe46c7ee83701d11ba5fed6
albizures <albizures3601@gmail.com>
2015-09-09 21:48:21 -0600

    multiplicacion de matrices de n*n terminada

--- js/multiplicacion.controller.js
+++ js/multiplicacion.controller.js
@@ -1,26 +1,53 @@
-
+'strict mode';
 angular.module('app')
   .controller('MultiplicacionCtrl',function ($scope) {
     $scope.agregar = true;
     $scope.quitar = true;
-    $scope.vector = {};
-    $scope.vector.n = 3;
-    $scope.getNumber = function (num) {
-      return new Array(num);
+    $scope.matriz = {};
+    $scope.matriz.n = 2;
+    $scope.getNumber = function (num,nombre) {
+      if($scope.matriz[nombre] && $scope.matriz[nombre].length){
+        if($scope.matriz[nombre].length != num ){
+          $scope.matriz[nombre] = new Array(num);
+        }
+      }else{
+        $scope.matriz[nombre] = new Array(num);
+      }
+
+      return $scope.matriz[nombre];
     }
     $scope.cambiarFilas = function (cambio) {
-      if(($scope.vector.n < 3 && cambio == -1) || ($scope.vector.n > 3 && cambio == 1)) return;
-      $scope.vector.n += cambio;
-      if($scope.vector.n < 3 ){
+      if(($scope.matriz.n < 3 && cambio == -1) || ($scope.matriz.n > 3 && cambio == 1)) return;
+      $scope.matriz.n += cambio;
+      if($scope.matriz.n < 3 ){
         $scope.quitar = false;
       }else{
         $scope.quitar = true;
       }
-      if($scope.vector.n > 3){
+      if($scope.matriz.n > 3){
         $scope.agregar = false;
       }else{
         $scope.agregar = true;
       }
+    }
+    $scope.$watchCollection('matriz.primera',resolver);
+    $scope.$watchCollection('matriz.segunda',resolver);
+    function resolver() {
+      $scope.matriz.resultado = new Array($scope.matriz.n * $scope.matriz.n);
+      for(var index = 0 ; index < $scope.matriz.n * $scope.matriz.n; index+=$scope.matriz.n ){
+        
+        var temp = $scope.matriz.primera.slice(index, index + $scope.matriz.n );
 
+          for(var index2 = 0 ; index2 < temp.length; index2++){
+
+            var temp2 = $scope.matriz.segunda.slice(index2*$scope.matriz.n,(index2*$scope.matriz.n) + $scope.matriz.n);
+            for(var index3 = 0; index3 < temp2.length; index3++){
+
+              var suma = Number(temp[index2] || 0)  * Number(temp2[index3] || 0);
+              $scope.matriz.resultado[index + index3] = isNaN(Number($scope.matriz.resultado[index + index3]))? 0 : $scope.matriz.resultado[index + index3];
+              $scope.matriz.resultado[index + index3] = $scope.matriz.resultado[index + index3] + suma;
+            }
+          }
+      }
     }
   });
